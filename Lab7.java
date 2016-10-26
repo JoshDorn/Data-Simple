1c9b56d801b40785455016ed2709717c3b7a40c3
Josh Dorn <jjd70@pitt.edu>
2016-10-26 14:40:46 -0400

    Added comments to main.

--- Lab7.java
+++ Lab7.java
@@ -1,39 +1,21 @@
-// CS 401 Fall 2016 Lab 7 Main Program
-// Your job is to complete this program so that it runs correctly.
-// The Movie class and MovieDB class have already been completed for you.
-// Utilizing data abstraction, you can access/use these classes without
-// having to know their implementation details.  However, since you have
-// the code for them you certainly can look at the implementations.
-// You just need to write the correct code in the 3 passages below.  Some
-// comments indicate what you need to do in each case.
+//Simple Movie Database
 import java.util.*;
 import java.io.*;
 public class Lab7
 {
 	public static void main(String [] args) throws IOException
 	{
-		MovieDB movies = new MovieDB(10); // Create MovieDB object.  The
-					// size is set at 10, meaning it can hold up to 10
-					// movies.  If we wanted (as discussed in lecture) we
-					// could allow for it to be resized so it could hold
-					// an arbitrary number of movies.
-		loadData(movies);		// input movie data from file
-		getCommands(movies);	// interact with user 
-		saveData(movies);		// save movie data back to file
+		MovieDB movies = new MovieDB(10);	// Create MovieDB object.
+		loadData(movies);			// input movie data from file
+		getCommands(movies);			// interact with user 
+		saveData(movies);			// save movie data back to file
 	}
 
 	public static void loadData(MovieDB movies) throws IOException
 	{
 		// Note the syntax below for creating a Scanner to a file
 		Scanner S = new Scanner(new FileInputStream("movieFile.txt"));
-	
-		// *** CODE SEGMENT 1 *** //
-		// Complete this method in the following way:
-		// Read in the number of movies from the file
-		// For each movie read the data from the file and create a Movie
-		// object
-		// Add the Movie to the MoviesDB object (movies) using the appropriate
-		// method (see MovieDB class)
+		
 		int numOfMovies = Integer.parseInt(S.nextLine());
 		for (int i = 0; i < numOfMovies; i++)
 		{
@@ -42,8 +24,7 @@ public class Lab7
 			String movieStudio = S.nextLine();
 			String movieGross = S.nextLine();
 			double movieMoney = Double.parseDouble(movieGross);
-			
-
+		
 			Movie stuff = new Movie (movieName, movieDirector, movieStudio, movieMoney);
 			movies.addMovie(stuff);
 		}
@@ -67,12 +48,6 @@ public class Lab7
 			}
 			else if (choice.equals("2"))
 			{
-				// *** CODE SEGMENT 2 *** //
-				// Complete this choice in the following way:
-				// Prompt for and read in each of the values needed
-				// for the new Movie object (3 strings, 1 double)
-				// Create a new Movie object and then add it to the
-				// MovieDB object (movies) using the correct method.
 				System.out.println("What is the title of the movie?");
 					String title = inScan.nextLine();
 				System.out.println("Who is the director?");
@@ -88,12 +63,7 @@ public class Lab7
 			}
 			else if (choice.equals("3"))
 			{
-				// *** CODE SEGMENT 3 *** //
-				// Complete this choice in the following way:
-				// Ask the user for the movie name and read it in
-				// Call the appropriate method in the MovieDB object
-				// (movies) to find the Movie and return it
-				// Show the movie's info (or indicate it is not found)
+	
 				System.out.println("What is the title of the movie you are looking for?");
 				Scanner find = new Scanner(System.in);
 				String title = (find.nextLine());
@@ -122,10 +92,6 @@ public class Lab7
 	public static void saveData(MovieDB movies) throws IOException
 	{
 		PrintWriter P = new PrintWriter("movieFile.txt");
-		// Note that we are printing the entire DB to the file with
-		// one statement.  This is because the toStringFile() method
-		// of the MovieDB class calls the toStringFile() method of
-		// each Movie within the DB.
 		P.print(movies.toStringFile());
 		P.close();
 	}
